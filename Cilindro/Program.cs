e9e8d87961f86a0acd83527a6564d4895aefafcb
Ricky-Balboa <ricardo.m.leal@hotmail.com>
2018-02-22 12:32:56 +0000

    exercicio de aula Cilindro

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -9,7 +9,23 @@ namespace Cilindro
     class Program
     {
         static void Main(string[] args)
+            
+
         {
+            string aux;
+            double a, r, V, S;
+            const double pi = 3.1415926;
+                Console.WriteLine("insira Altura ");
+                aux = Console.ReadLine();
+            a = Convert.ToDouble(aux);
+            Console.WriteLine("Insira Raio");
+            aux = Console.ReadLine();
+            r = Convert.ToDouble(aux);
+            V = pi * r * r * a;
+            S = 2 * pi * r * (r + a);
+            Console.WriteLine("V= " + V);
+            Console.WriteLine("S= " + S);
+
         }
     }
 }
